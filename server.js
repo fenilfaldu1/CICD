c540ab33004830bb12d32c86f69b2063857ec077
fenilfaldu1 <155763749+fenilfaldu1@users.noreply.github.com>
2024-02-06 12:10:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const auth = (req,res,  next) => {
 	next();
 }
 app.get('/',  (req, res) => {
-	res.send("Hello World!,This is Fenil's First pipeline");
+	res.send("This is Fenil's First pipeline");
   })
 app.get('/testgetrequest',  (req, res) => {
 	res.send('Hello World!');

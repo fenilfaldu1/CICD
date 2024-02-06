aca968f6f2a05e37df7b11e5c8796a24913b229d
fenilfaldu1 <155763749+fenilfaldu1@users.noreply.github.com>
2024-02-06 11:55:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 
-const port = 5000
+const port = 6000
 const auth = (req,res,  next) => {
 	const headers = req.headers;
 	if (headers['x-dsn']!='abcd-abcd')
